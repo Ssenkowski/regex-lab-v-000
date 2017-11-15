ab67132251d5ce1d087b40138dfb2878184d7d46
Steven Senkowski <scsenkowski@gmail.com>
2017-11-15 17:38:33 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -7,13 +7,13 @@ def words_starting_with_un_and_ending_with_ing(text)
 end
 
 def words_five_letters_long(text)
-
+ text.scan(/ /)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+ text.match(/ /)
 end
 
 def valid_phone_number?(phone)
-
+ phone.match(/ /)
 end
